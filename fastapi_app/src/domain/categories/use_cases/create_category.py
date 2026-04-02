91bc2356f6905a019f35af0a7b8737d65d44da6c
gohy907 <gohy2006@mail.ru>
2026-04-02 18:00:02 +0400

    fix: check for author existence in create category use case

--- fastapi_app/src/domain/categories/use_cases/create_category.py
+++ fastapi_app/src/domain/categories/use_cases/create_category.py
@@ -3,14 +3,20 @@ from schemas.categories import CategoryResponse, CategoryCreate
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from domain.users.use_cases.get_user_by_id import GetUserByIdUseCase
+
 
 class CreateCategoryUseCase:
 
     def __init__(self):
+
+        self.get_user_use_case = GetUserByIdUseCase()
         pass
 
     async def execute(self, session: AsyncSession, category_in: CategoryCreate) -> CategoryResponse:
         repo = CategoryRepository(session)
+
+        await self.get_user_use_case.execute(session, category_in.author_id)
 
         category_data = category_in.model_dump()
         category = await repo.create(category_data)
--- fastapi_app/src/repositories/users.py
+++ fastapi_app/src/repositories/users.py
@@ -12,13 +12,6 @@ class UserRepository(BaseRepository[User]):
     def __init__(self, session: AsyncSession):
         super().__init__(User, session)
 
-    # async def get_by_l(self, login: str) -> Optional[User]:
-    #     result = await self.get(login)
-    #     return result.scalar_one_or_none()
-        # query = select(User).where(User.login == login)
-        # result = await self.session.execute(query)
-        # return result.scalar_one_or_none()
-
     async def does_user_exist_by_login(self, login: str) -> bool:
         query = select(User).where(
             User.login == login)
