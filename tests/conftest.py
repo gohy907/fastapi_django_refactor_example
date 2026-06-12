37b6e6892a476e88791d55fd9dcf690f3a1b9ea7
gohy907 <gohy2006@mail.ru>
2026-06-12 11:22:27 +0400

    feat: add new tests to User

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import pytest_asyncio
 
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from testcontainers.postgres import PostgresContainer
 from httpx import AsyncClient, ASGITransport
@@ -8,6 +9,9 @@ from httpx import AsyncClient, ASGITransport
 from main import app
 from src.core.config import settings
 from src.core.db import Base, get_db
+
+from src.domain.users.use_cases.create_user import CreateUserUseCase
+from src.schemas.users import UserCreate
 
 
 @pytest.fixture(scope="session")
@@ -25,14 +29,41 @@ def db_url(pg_container):
     )
 
 
-@pytest_asyncio.fixture
-async def async_client(db_url):
+@pytest.fixture(scope="session")
+def sync_engine(db_url):
+    sync_url = db_url.replace("+asyncpg", "")
+    engine = create_engine(sync_url)
+    Base.metadata.create_all(engine)
+    yield engine
+    Base.metadata.drop_all(engine)
+
+
+@pytest_asyncio.fixture(scope="session")
+async def created_user(sync_engine, db_url):
+
     engine = create_async_engine(db_url)
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    async with async_sessionmaker(
+        bind=engine,
+        expire_on_commit=False,
+        class_=AsyncSession,
+    )() as session:
+        use_case = CreateUserUseCase()
+        user = await use_case.execute(
+            session=session,
+            user_in=UserCreate(login="notrealuser", password="password"),
+        )
+    await engine.dispose()
+    return user
+
+
+@pytest_asyncio.fixture
+async def async_client(db_url, sync_engine):
+    engine = create_async_engine(db_url)
 
     SessionFactory = async_sessionmaker(
-        bind=engine, expire_on_commit=False, class_=AsyncSession,
+        bind=engine,
+        expire_on_commit=False,
+        class_=AsyncSession,
     )
 
     async def override_get_db():
@@ -52,6 +83,18 @@ async def async_client(db_url):
         yield ac
 
     app.dependency_overrides.clear()
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
     await engine.dispose()
+
+
+@pytest_asyncio.fixture
+async def auth_client(async_client, created_user):
+    from jose import jwt
+    from src.services.auth import SECRET_AUTH_KEY, AUTH_ALGORITHM
+
+    token = jwt.encode(
+        claims={"sub": created_user.login},
+        key=SECRET_AUTH_KEY.get_secret_value(),
+        algorithm=AUTH_ALGORITHM,
+    )
+    async_client.headers["Authorization"] = f"Bearer {token}"
+    yield async_client
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,4 @@
-from src.schemas.users import UserResponse
+import uuid
 from http import HTTPStatus
 
 
@@ -6,10 +6,7 @@ class TestCreateUser:
     async def test_create(self, async_client):
         response = await async_client.post(
             "/users/create",
-            json={
-                "login": "user",
-                "password": "aboba",
-            },
+            json={"login": "user", "password": "aboba"},
         )
         assert response.status_code == HTTPStatus.CREATED
 
@@ -34,3 +31,44 @@ class TestCreateUser:
             "/users/create", json={"login": "абоба", "password": "aboba"}
         )
         assert response.status_code == HTTPStatus.UNPROCESSABLE_CONTENT
+
+
+class TestGetUserById:
+    async def test_get_by_id(self, auth_client, created_user):
+        response = await auth_client.get(f"/users/id/{created_user.id}")
+        assert response.status_code == HTTPStatus.OK
+
+        data = response.json()
+        assert data["login"] == created_user.login
+        assert data["id"] == str(created_user.id)
+
+    async def test_get_by_id_nonexistent(self, auth_client, created_user):
+
+        nonexistent_id = uuid.UUID(int=created_user.id.int ^ 1)
+        # если использовать uuid.uuid4(), то раз в триллион запусков тест упадёт
+        # я не хочу полагаться на рандом)
+        response = await auth_client.get(f"/users/id/{nonexistent_id}")
+        assert response.status_code == HTTPStatus.NOT_FOUND
+
+    async def test_get_by_id_unauthorized(self, async_client, created_user):
+        response = await async_client.get(f"/users/id/{created_user.id}")
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+class TestGetUserByLogin:
+    async def test_get_by_login(self, auth_client, created_user):
+        response = await auth_client.get(f"/users/login/{created_user.login}")
+        assert response.status_code == HTTPStatus.OK
+
+        data = response.json()
+        assert data["login"] == created_user.login
+        assert data["id"] == str(created_user.id)
+
+    async def test_get_by_login_nonexistent(self, auth_client, created_user):
+        nonexistent_login = "koitese"
+        response = await auth_client.get(f"/users/login/{nonexistent_login}")
+        assert response.status_code == HTTPStatus.NOT_FOUND
+
+    async def test_get_by_login_unauthorized(self, async_client, created_user):
+        response = await async_client.get(f"/users/login/{created_user.login}")
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
